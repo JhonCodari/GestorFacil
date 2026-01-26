e77368b58a699f865313fe0aceb0152c79c13841
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2026-01-26 03:09:21 -0300

    cria a aplicação base

---
+++ src/main/java/com/JhonCodari/GestorFacil/GestorFacilApplication.java
@@ -0,0 +1,13 @@
+package com.JhonCodari.GestorFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JhonCodari/GestorFacil/GestorFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JhonCodari.GestorFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
