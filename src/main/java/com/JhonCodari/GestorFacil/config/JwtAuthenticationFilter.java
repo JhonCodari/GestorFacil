130af0597bf577617f5fc300aeceeddf499a1712
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2026-02-18 23:39:32 -0300

    ajusta validação de token revogado

--- src/main/java/com/JhonCodari/GestorFacil/config/JwtAuthenticationFilter.java
+++ src/main/java/com/JhonCodari/GestorFacil/config/JwtAuthenticationFilter.java
@@ -4,10 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.JhonCodari.GestorFacil.exception.TokenNaBlacklistException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,9 +19,13 @@ import java.util.ArrayList;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final StringRedisTemplate redisTemplate;
 
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+    public JwtAuthenticationFilter(
+            JwtTokenProvider jwtTokenProvider,
+            StringRedisTemplate redisTemplate) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -34,6 +41,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             String token = authHeader.substring(7);
             
             if (jwtTokenProvider.validarToken(token)) {
+                if (Boolean.TRUE.equals(redisTemplate.hasKey(token))) {
+                    throw new TokenNaBlacklistException("Token foi revogado");
+                }
+                
                 String email = jwtTokenProvider.extrairSubject(token);
                 
                 UsernamePasswordAuthenticationToken authentication = 
