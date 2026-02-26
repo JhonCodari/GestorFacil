56ecffae70e57d0831b3fc4fd22b9c4d783e9c94
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2026-02-26 16:40:45 -0300

    cria bean de configuração para a api de cambio

--- src/main/java/com/JhonCodari/GestorFacil/config/RestClientConfig.java
+++ src/main/java/com/JhonCodari/GestorFacil/config/RestClientConfig.java
@@ -11,10 +11,21 @@ public class RestClientConfig {
     @Value("${integracao.banco.url-base}")
     private String urlBaseBanco;
 
+    @Value("${integracao.cambio.url-base}")
+    private String urlBaseCambioApi;
+
     @Bean
     public RestClient contaBancariaRestClient() {
         return RestClient.builder()
             .baseUrl(urlBaseBanco)
+            .defaultHeader("Accept", "application/json")
+            .build();
+    }
+
+    @Bean
+    public RestClient cambioRestClient() {
+        return RestClient.builder()
+            .baseUrl(urlBaseCambioApi)
             .defaultHeader("Accept", "application/json")
             .build();
     }
